e6f43b302c537fa83432d2d79e3465b02388d9e8
lsh02002 <lsh02002@hanmail.net>
2026-03-17 14:41:09 +0900

    Update sehodiary-api.tsx

--- src/api/sehodiary-api.tsx
+++ src/api/sehodiary-api.tsx
@@ -52,7 +52,7 @@ api.interceptors.response.use(
       showToast(error.response.data.detailMessage);
     }
     // 그 외의 에러도 같이 로깅
-    if (error.message === "Network Error") {
+    else {
       showToast(error.message);
     }
 
