fb3a0655f4e202d3c2b9f775db9c9b3e87779e32
lsh02002 <lsh02002@hanmail.net>
2026-04-12 16:18:52 +0900

    Update Firebase.ts

--- src/firebase/Firebase.ts
+++ src/firebase/Firebase.ts
@@ -10,6 +10,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+console.log(firebaseConfig);
+
 export const firebase = initializeApp(firebaseConfig);
 
 export async function getFirebaseMessaging() {
